e6a4dec56fef0019a8b767a270d3621aa36ee92a
Manish <maknish1993@gmail.com>
2023-02-09 15:19:18 +0530

    Initial commit

--- src/Components/ChooseState/index.jsx
+++ src/Components/ChooseState/index.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 import cities from "../../Data/in.json";
 import { UseWeatherAppContext } from '../../Context/Context';
 
 const ChooseState = () => {
-    console.log(UseWeatherAppContext());
+    const {state:{city}, dispatch} = UseWeatherAppContext();
+    console.log("UseWeatherAppContext", UseWeatherAppContext)
+
+    const handleChange = (e) => {
+        const selectedCity = cities.filter((city) => {
+            return city.city = e.target.value
+        })[0]
+        // console.log(selectedCity);
+        dispatch({
+            type:'SET_CITY',
+            payload:selectedCity
+        })
+    }
+    //API call
+    const APIKEY = "5bcda7dbb9b0daa5e2d46b939bfe9124";
+    let lat = city && city.lat ? city.lat : "";
+    let long = city && city.lng ? city.lng : "";
+    let exculde = 'hourly, minutely';
   return (
     <>
         <div className='stateWrap'>
-            <select className='stateMenu'>
+            <select className='stateMenu' defaultValue={city} onChange={handleChange}>
                 {
                     cities && cities.length > 0 && cities.map((city) => {
                         return(
